15ee5fba0d52dbf7b88a04a897c24d14817ec071
choijaegwon <68246962+choijaegwon@users.noreply.github.com>
2020-07-16 02:55:07 +0900

    StudentTest 실행 클래스 만들기
    02~08줄 main( )함수
    03줄 Student 클래스 생성
    PS. Student3와 같은 패키지에 있기 때문에 불러와서 쓰는 것이다.

---
+++ 클래스와 객체1/StudentTest.java
@@ -0,0 +1,9 @@
+public class StudentTest {
+	public static void main(String[] args) {
+		Student3 studentAhn = new Student3();
+		studentAhn.studentName = "안승연";
+		
+		System.out.println(studentAhn.studentName);
+		System.out.println(studentAhn.getStudentName( ));
+	}
+}
