5484e97173591d78b237e0df2c3cc60e90c639de
choijaegwon <68246962+choijaegwon@users.noreply.github.com>
2020-07-15 13:14:08 +0900

    학생 이름과 주소 출력하는 메서드 만들기
    09~11줄 메서드 추가

---
+++ 클래스와 객체1/Student2.java
@@ -0,0 +1,11 @@
+public class Student2 {
+	int studentID;
+	String studentName;
+	int grade;
+	String address;
+	
+	public void showStudentInfo( ) {
+		System.out.println(studentName+","+address);//이름,주소 출력
+	}
+
+}
