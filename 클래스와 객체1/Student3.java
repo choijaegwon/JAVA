6fe06481bae521e912252f5aeae228bf8c7daca5
choijaegwon <68246962+choijaegwon@users.noreply.github.com>
2020-07-18 14:54:35 +0900

    Student 클래스에 main() 함수 추가하기
    11~17줄 main()함수
    12줄 Student3 클래스 생성

---
+++ 클래스와 객체1/Student3.java
@@ -0,0 +1,18 @@
+public class Student3 {
+	int sutdentID;
+	String studentName;
+	int grade;
+	String address;
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public static void main(String[] args) {
+		Student3 studentAhn = new Student3( );
+		studentAhn.studentName = "안연수";
+		
+		System.out.println(studentAhn.studentName);
+		System.out.println(studentAhn.getStudentName());
+	}
+}
