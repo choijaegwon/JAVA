e939e76353d66c2a059e7b3fe62d48cee1da57eb
choijaegwon <68246962+choijaegwon@users.noreply.github.com>
2020-07-20 15:01:40 +0900

    학생 클래스 만들기(2)

---
+++ 클래스와 객체1/참조 자료형/Student2.java
@@ -0,0 +1,10 @@
+package reference;
+
+public class Student2 {
+	int studentID;
+	String studentName;
+	int koreaScore;
+	int mathScore;
+	String koreaSubject; //과목이름 변수추가
+	String mathSubject; //과목이름 변수추가
+}
