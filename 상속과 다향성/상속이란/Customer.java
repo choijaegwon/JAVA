9dd49b448b4aa83c1cce6e8b62bf03f22c64fda4
choijaegwon <68246962+choijaegwon@users.noreply.github.com>
2020-07-27 15:55:01 +0900

    Customer 클래스 구현하기
    04~08줄 멤버 변수
    10~13줄 디폴트 생성자
    15~18줄 보너스포인트 적립, 지불 계싼 메서드
    20~22줄 고객 정보를 반환하는 메서드

--- 상속과 다향성/상속이란/Customer.java
+++ 상속과 다향성/상속이란/Customer.java
@@ -1,12 +1,12 @@
 package inheritance;
 
 public class Customer {
-	private int customerID; //고객 아이디, 멤버 변수
-	private String customerName; //고객 이름, 멤버 변수
-	private String customerGrade; //고객 등급, 멤버 변수
+	protected int customerID; //고객 아이디, 멤버 변수
+	protected String customerName; //고객 이름, 멤버 변수
+	protected String customerGrade; //고객 등급, 멤버 변수
 	int bonusPoint; //보너스 포인트, 멤버 변수
 	double bonusRatio; //적립 비율 , 멤버 변수
-	
+
 	public Customer() {
 		customerGrade = "SILVER"; //기본등급, 디폴트 생성자
 		bonusRatio = 0.01; //보너스 포인트 기본 적립 비율, 디폴트 생성자
@@ -17,7 +17,31 @@ public class Customer {
 		return price;
 	}
 	
-	public String showCustomerInfo( ) {
+	public String showCustomerInfo( ) { //고객 정보를 반환하는 메서드
 		return customerName + "님의 등급은 " + customerGrade + "이며, 보너스 포인트는" + bonusPoint + "입니다,";
+	}
+	
+	public int getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCustomerGrade() {
+		return customerGrade;
+	}
+
+	public void setCustomerGrade(String customerGrade) {
+		this.customerGrade = customerGrade;
 	}
 }
