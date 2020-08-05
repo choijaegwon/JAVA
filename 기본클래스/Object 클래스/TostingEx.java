3e7ae2424eaef3fd9c3465cef1d7ab6ff63c11fa
choijaegwon <68246962+choijaegwon@users.noreply.github.com>
2020-08-05 14:14:25 +0900

    Object 클래스의 toString() 메서드 사용하기

---
+++ 기본클래스/Object 클래스/TostingEx.java
@@ -0,0 +1,20 @@
+package object;
+
+class Book{
+	int bookNumber;
+	String bookTitle;
+	
+	Book(int bookNumber, String bookTitle){ //책 번호와 제목을 매개변수로 입력받는 생성자
+		this.bookNumber = bookNumber;
+		this.bookTitle = bookTitle;
+	}
+}
+
+public class TostingEx {
+	public static void main(String[] args) {
+		Book book1 = new Book(200, "개미");
+		
+		System.out.println(book1);//인스턴스 정보(클래스 이름.주소값)
+		System.out.println(book1.toString()); //toString()메서드로 인스턴스 정보 (클래스 이름. 주소 값)를 보여줌
+	}
+}
