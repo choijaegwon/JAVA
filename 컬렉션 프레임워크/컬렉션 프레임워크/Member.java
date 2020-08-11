788e594b784937cd48cfe44c2ecd867b768c0b6e
choijaegwon <68246962+choijaegwon@users.noreply.github.com>
2020-08-11 12:42:00 +0900

    HashSet 활용하기

--- 컬렉션 프레임워크/컬렉션 프레임워크/Member.java
+++ 컬렉션 프레임워크/컬렉션 프레임워크/Member.java
@@ -27,4 +27,21 @@ public class Member{
 	public String toString(){   //toString 메소드 오버로딩
 		return memberName + " 회원님의 아이디는 " + memberId + "입니다";
 	}
+
+	@Override
+	public int hashCode() {
+		return memberId; //hashCode() 메서드가 회원 아이디를 반환하도록 재정의
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if( obj instanceof Member){
+			Member member = (Member)obj;
+			if( this.memberId == member.memberId ) //매개변수로 받은 회원 아이디가 자신의 회원 아이디와 같다면 true 반환
+				return true;
+			else 
+				return false;
+		}
+		return false;
+	}
 }
