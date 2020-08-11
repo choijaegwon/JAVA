5623ed21c99b76c2383ba1fd8f6d0e13b6c3300d
choijaegwon <68246962+choijaegwon@users.noreply.github.com>
2020-08-11 12:51:17 +0900

    Comparable 인터페이스 구현하기

--- 컬렉션 프레임워크/컬렉션 프레임워크/Member.java
+++ 컬렉션 프레임워크/컬렉션 프레임워크/Member.java
@@ -1,7 +1,6 @@
 package collection;
 
-public class Member{
-
+public class Member implements Comparable<Member>{
 	private int memberId;        //회원 아이디
 	private String memberName;   //회원 이름
 
@@ -43,5 +42,12 @@ public class Member{
 				return false;
 		}
 		return false;
+	}
+
+	@Override
+	public int compareTo(Member member) {
+		
+		return (this.memberId - member.memberId);   //오름차순
+		//return (this.memberId - member.memberId) *  (-1);   //내림 차순
 	}
 }
