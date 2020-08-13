92d5e776860b20900c31d5716b7e43f66fffc8ae
choijaegwon <68246962+choijaegwon@users.noreply.github.com>
2020-08-13 17:14:43 +0900

    인스턴스 내부 클래스 예제

---
+++ 내부 클래스. 람다식, 스트림/내부 클래스/InnerTest.java
@@ -0,0 +1,36 @@
+package innerclass;
+
+class OutClass { //외부 클래스
+	private int num = 10; //외부 클래스 private 변수 
+	private static int sNum = 20; //외부 클래스 정적 변수
+	
+	private InClass inClass; //내부 클래스 자료형 변수를 먼저 선언
+	
+	public OutClass(){ //외부 클래스 디폴트 생성자. 외부클래스가 생성된후에 내부 클래스 생성 가능
+		inClass = new InClass(); // 내부 클래스 생성
+	}
+	
+	class InClass{ //인스턴스 내부 클래스
+		int inNum = 100; //내부 클래스의 인스턴스 변수
+		//static int sInNum = 200;  //에러 남
+		
+		void inTest(){
+			System.out.println("OutClass num = " +num + "(외부 클래스의 인스턴스 변수)");
+			System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 스태틱 변수)");
+		}
+		
+	    //static void sTest(){  //에러 남
+	    //}
+	}
+	public void usingClass(){
+		inClass.inTest();
+	}
+}
+
+public class InnerTest{	
+	public static void main(String[] args){	
+		OutClass outClass = new OutClass();
+		System.out.println("외부 클래스 이용하여 내부 클래스 기능 호출");
+		outClass.usingClass();    // 내부 클래스 기능 호출
+	}
+}
