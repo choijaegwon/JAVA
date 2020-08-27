4e6b14222357bfd582d5c536af9677872fb8d5cd
choijaegwon <68246962+choijaegwon@users.noreply.github.com>
2020-08-27 14:28:39 +0900

    PlayerLever 추상 클래스 구현하기

---
+++ Do it! 자바 프로그래밍 입문/추상 클래스/템플릿 메서드 응용하기/PlayerLevel.java
@@ -0,0 +1,16 @@
+package gameLevel;
+
+public abstract class PlayerLevel {
+	public abstract void run( );
+	public abstract void jump( );
+	public abstract void turn( );
+	public abstract void showLevelMessage( );
+	
+	final public void go(int count) {//재정의하면 안 되므로 final로 선언
+		run( );
+		for(int i = 0; i < count; i++) {
+			jump( );
+		}
+		turn( );	
+	}
+}
